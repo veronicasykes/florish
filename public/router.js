76dfa936a70011ef2833e1121ce3f5f6e1f12371
Veronica Sykes <vasykes@gmail.com>
2016-02-16 11:07:07 -0800

    creates router.js

---
+++ public/router.js
@@ -0,0 +1,48 @@
+angular.module('Florish')
+	.config(MainRouter)
+	.config(interceptor)
+
+function interceptor($httpProvider) {
+	$httpProvider.interceptors.push('authInterceptorFactory')
+}
+
+function MainRouter($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/login')
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: 'partials/home.html',
+			controller: 'UsersController as usersCtrl'
+		})
+		.state('login', {
+			url: '/login',
+			templateUrl: 'partials/login.html',
+			controller: 'UsersController as usersCtrl'
+		})
+		.state('signup', {
+			url: '/signup',
+			templateUrl: 'partials/signup.html',
+			controller: 'UsersController as usersCtrl'
+		})
+		.state('loggedOut', {
+			url: '/loggedOut',
+			templateUrl: 'partials/home.html',
+			controller: 'UsersController as usersCtrl'
+		})
+		.state('detail', {
+			url: '/products/:productId',
+			templateUrl: 'partials/product-detail.html',
+			controller: 'ProductDetailsController as productDetailsCtrl'
+		})
+		.state('shop', {
+			url: '/shop',
+			templateUrl: 'partials/shop.html',
+			controller: 'ProductsController as productsCtrl'
+		})
+		.state('search', {
+			url: '/search',
+			templateUrl: 'partials/search.html',
+			controller: 'ProductsController as productsCtrl'
+		})
+
+}
