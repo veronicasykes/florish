81b3072d8c8cd3fce7cfc40f3cead640f6c36ffe
Veronica Sykes <vasykes@gmail.com>
2016-02-18 17:17:23 -0800

    trying to fix heroku

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -2,8 +2,8 @@ var express = require('express')
 var apiRouter = express.Router() //get an instance of express router
 var usersController = require('../controllers/usersController')
 var productsController = require('../controllers/productsController')
-var auth = require('../auth.js')
-var stripe = require('stripe')(auth.key)
+// var auth = require('../auth.js')
+var stripe = require('stripe')(process.env.stripeKey)
 var User = require('../models/User')
 
 
