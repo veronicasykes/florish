17148eadd54d787f308b71e389cec35dd8f96668
Veronica Sykes <vasykes@gmail.com>
2016-02-18 18:17:10 -0800

    fixes comma error

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var express 	= require('express'),
 	apiRouter 	= require('./api/routes/user_routes'),
 	aws				= require('aws-sdk'),
 	mongoUri =  process.env.MONGOLAB_URI || 'mongodb://localhost:27017/florish',
-	AWS_ACCESS_KEY 	= process.env.AWS_ACCESS_KEY
-	AWS_SECRET_KEY 	= process.env.AWS_SECRET_KEY
+	AWS_ACCESS_KEY 	= process.env.AWS_ACCESS_KEY,
+	AWS_SECRET_KEY 	= process.env.AWS_SECRET_KEY,
 	S3_BUCKET 	 	= process.env.S3_BUCKET
 
 
